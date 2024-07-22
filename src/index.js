d908b1b7a217f3c1888c27c623b160711d0ef4ec
Yong Zhi <79264302+pohyongzhi@users.noreply.github.com>
2024-07-22 23:46:24 +0800

    Added basic functionalities

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+// import { compareAsc, format } from "date-fns";
+
 class ToDo {
     constructor(title, description, dueDate, priority) {
         this.title = title;
@@ -26,9 +28,6 @@ class Project {
         projectsBtnGrp.innerHTML = "";
 
         const size = Object.keys(this.projectList).length;
-
-        console.log(this.projectList); // Debug log
-        console.log(size); // Debug log
 
         for (let i = 0; i < size; i++) {
             const key = Object.keys(this.projectList)[i];
@@ -73,17 +72,87 @@ class LogicController {
         this.addTaskCtrl();
         this.addProjectCtrl();
         this.delProjectCtrl();
+        project.populateProjectButtons();
     }
 
-    addTaskCtrl() {}
+    addTaskCtrl() {
+        const addTaskBtn = document.querySelector("#add-task-btn");
+        const closeProjectFormBtn = document.querySelector(
+            ".task-form-close-btn"
+        );
+        const taskForm = document.querySelector(".add-task-form");
+        const addTaskForm = document.querySelector(".task-form-container");
+
+        // Show form
+        addTaskBtn.addEventListener("click", () => {
+            addTaskForm.style.display = "flex";
+
+            // Get project list
+            const projectKeys = Object.keys(project.getProjectList());
+
+            // Get drop down id
+            const dropdown = document.querySelector("#taskProjectDropdown");
+
+            // Create and append the default option to the dropdown
+            const defaultOption = document.createElement("option");
+            defaultOption.value = "";
+            defaultOption.textContent = "Select an option";
+            defaultOption.selected = true;
+            defaultOption.disabled = true;
+            dropdown.appendChild(defaultOption);
+
+            projectKeys.forEach((key) => {
+                const option = document.createElement("option");
+                option.value = key;
+                option.textContent = key;
+                dropdown.appendChild(option);
+            });
+        });
+
+        // Close the form
+        closeProjectFormBtn.addEventListener("click", () => {
+            addTaskForm.style.display = "none";
+        });
+
+        // Add a task
+        taskForm.addEventListener("submit", (event) => {
+            event.preventDefault();
+
+            const formData = new FormData(taskForm);
+
+            const taskName = formData.get("taskName");
+            const taskDesc = formData.get("taskDescription");
+            const taskDueDate = formData.get("taskDueDate");
+            const taskPriority = formData.get("taskPriority");
+            const taskProject = formData.get("taskProject");
+
+            // Create toDo
+            const toDo = new ToDo(
+                taskName,
+                taskDesc,
+                taskDueDate,
+                taskPriority
+            );
+
+            // Add toDo
+            project.addToDo(taskProject, toDo);
+
+            // Re-display
+
+            // Hide the form
+            addTaskForm.style.display = "none";
+        });
+    }
 
     addProjectCtrl() {
         const addProjectBtn = document.querySelector("#add-project-btn");
-        const closeProjectFormBtn = document.querySelector(".close-btn");
-        const projectForm = document.querySelector(".project-form");
+        const closeProjectFormBtn = document.querySelector(
+            ".project-form-close-btn"
+        );
+        const projectForm = document.querySelector(".add-project-form");
         const addProjectForm = document.querySelector(".form-container");
 
-        // Show form logic
+        // Show form
         addProjectBtn.addEventListener("click", () => {
             addProjectForm.style.display = "flex";
         });
@@ -117,26 +186,41 @@ class LogicController {
     }
 
     delProjectCtrl() {
-        const delProjectBtn = document.querySelector("#del-project-btn");
+        // const delProjectBtn = document.querySelector("#del-project-btn");
+        // const delProjectForm = document.querySelector(".form-container");
+        // // Show form logic
+        // delProjectBtn.addEventListener("click", () => {});
     }
 }
 
 class DisplayController {
+    constructor(project) {
+        this.project = project;
+    }
+
     displayAllProject(projects) {}
 
     displayProject(project) {}
+
+    displayToDo(toDo) {
+        const content = document.querySelector("#id");
+
+        // Creation of card
+        const card = document.createAttribute("div");
+        card.className = "card";
+
+        // Appending of card
+    }
 }
+
+const project = new Project();
+const logicController = new LogicController(project);
+const displayController = new DisplayController(project);
 
 const todo1 = new ToDo("hello", "test", "12/02/23", "high");
 const todo2 = new ToDo("hello", "test", "12/02/23", "low");
 const todo3 = new ToDo("hello", "test", "12/02/23", "high");
 
-const project = new Project();
-const logicController = new LogicController();
-const displayController = new DisplayController();
-
-// project.populateProjectButtons();
-// project.deleteProject("test1");
-project.populateProjectButtons();
-
-project.addToDo("inbox", todo1);
+project.addToDo("today", todo1);
+project.addToDo("today", todo2);
+project.addToDo("today", todo3);
